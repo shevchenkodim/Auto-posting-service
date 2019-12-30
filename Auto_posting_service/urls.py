1b981d20dd6f8a61b3d6ad1e88e7b74e70846268
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2019-12-30 02:17:59 +0200

    Add SignUp/Login/Logout
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- Auto_posting_service/urls.py
+++ Auto_posting_service/urls.py
@@ -17,10 +17,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from first_page import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('first_page.urls'))
+    path('admin/',   admin.site.urls),
+    path('',         include('first_page.urls')),
+    path('signup/',  views.signup ,                   name='signup'),
+    path('login/',   auth_views.LoginView.as_view(),  name='login'),
+    path('logout/',  auth_views.LogoutView.as_view(), name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
