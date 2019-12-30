2bd6724e51e52799cca44a3e4af25e99e7d302f6
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2019-12-31 01:06:52 +0200

    Improve studio
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- studio/urls.py
+++ studio/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.studio, name="studio"),
+    path('/tasks/', views.tasks, name="tasks"),
+    path('/statistics/', views.statistics, name="statistics"),
+    path('/settings/', views.settings, name="settings"),
 ]
--- studio/views.py
+++ studio/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def studio(request):
+    return render(request, "studio/studio.html")
+
+def tasks(request):
+    return render(request, "studio/tasks.html")
+
+def statistics(request):
+    return render(request, "studio/statistics.html")
+
+def settings(request):
+    return render(request, "studio/settings.html")
