59b7bf0fe849d6809fa585928b516575dec3e225
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-01-10 23:25:12 +0200

    Improve Model Post
    :Release Notes:
    
    :Detailed Notes:
     Add fields
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- studio/models.py
+++ studio/models.py
@@ -1,22 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+import datetime
 User = get_user_model()
-
-
-class Post(models.Model):
-    user         = models.ForeignKey(
-                                    User,
-                                    verbose_name="Автор",
-                                    on_delete=models.CASCADE)
-    title        = models.CharField("Заголовок", max_length=100)
-    text         = models.TextField("Текст статьи")
-    images       = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True, null=True)
-    date_posting = models.DateTimeField("Дата", blank=True, null=True)
-    moderation   = models.IntegerField("Модерация", blank=True, null=True)
-
-    def __str__(self):
-        return self.title
 
 
 class SocialNetwork(models.Model):
@@ -35,3 +21,21 @@ class SocialNetworkTelegram(models.Model):
     name           = models.ForeignKey(SocialNetwork, null=False, blank=False, on_delete=models.CASCADE)
     name_channel   = models.CharField(null=False, blank=False, max_length=256)
     connect_result = models.BooleanField(null=False, default=False)
+
+
+class Post(models.Model):
+    user         = models.ForeignKey(
+                                    User,
+                                    verbose_name="Користувач",
+                                    on_delete=models.CASCADE)
+    sn_facebook      = models.ForeignKey(SocialNetworkFacebook, null=True, blank=True, on_delete=models.CASCADE)
+    sn_telegram      = models.ForeignKey(SocialNetworkTelegram, null=True, blank=True, on_delete=models.CASCADE)
+    title            = models.CharField("Заголовок", max_length=100)
+    text             = models.TextField("Текст поста")
+    images           = models.ImageField("Картинка", upload_to=settings.MEDIA_ROOT, blank=True, null=True)
+    date_posting     = models.DateTimeField("Дата час коли викласти", blank=True, null=True)
+    date_post_create = models.DateTimeField("Дата создания", auto_now_add=True, blank=True, null=True)
+    connect_result = models.BooleanField(null=False, default=False)
+
+    def __str__(self):
+        return self.title
