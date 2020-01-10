493c2a67702d7fd9b898cee9112e02c729edef4e
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-01-10 22:37:54 +0200

    Create Social Network Facebook
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- studio/models.py
+++ studio/models.py
@@ -4,19 +4,25 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Post(models.Model):
-    user = models.ForeignKey(
-            User,
-            verbose_name="Автор",
-            on_delete=models.CASCADE)
-    title = models.CharField("Заголовок", max_length=100)
-    text = models.TextField("Текст статьи")
-    images = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True, null=True)
+    user         = models.ForeignKey(
+                                    User,
+                                    verbose_name="Автор",
+                                    on_delete=models.CASCADE)
+    title        = models.CharField("Заголовок", max_length=100)
+    text         = models.TextField("Текст статьи")
+    images       = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True, null=True)
     date_posting = models.DateTimeField("Дата", blank=True, null=True)
-    moderation = models.IntegerField("Модерация", blank=True, null=True)
+    moderation   = models.IntegerField("Модерация", blank=True, null=True)
 
     def __str__(self):
         return self.title
 
 class SocialNetwork(models.Model):
     name = models.CharField("Назва", max_length=100)
-    type = models.CharField("Тип", max_length=100)    
+    type = models.CharField("Тип", max_length=100)
+
+class SocialNetworkFacebook(models.Model):
+    name           = models.ForeignKey(SocialNetwork, null=False, blank=False, on_delete=models.CASCADE)
+    login          = models.CharField(null=False, blank=False, max_length=100)
+    password       = models.CharField(null=False, blank=False, max_length=100)
+    connect_result = models.BooleanField(null=False, default=False)
