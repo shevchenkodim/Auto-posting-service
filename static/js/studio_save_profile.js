dd6c93f2d48be6d8c1e0806bbd0ef5d5461150ba
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-02-24 14:18:59 +0200

    Improve save profile info
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- static/js/studio_save_profile.js
+++ static/js/studio_save_profile.js
@@ -21,11 +21,13 @@ $('.save-profile').on('click', function(){
     formData.append('last_name', last_name);
     account_email = $('#account_email').val();
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    if(reg.test(account_email) == false) {
-        $('.iframe-text').text('Введите корректный e-mail!');
-        $('#exampleModalCenter').modal('show');
-        $('#progress').attr("class", "fi fi-check")
-        return false;
+    if (account_email > 0){
+        if(reg.test(account_email) == false) {
+            $('.iframe-text').text('Введите корректный e-mail!');
+            $('#exampleModalCenter').modal('show');
+            $('#progress').attr("class", "fi fi-check")
+            return false;
+        }
     }
     formData.append('account_email', account_email);
     csrf_token =  $('input[name="csrf_token"]').attr('value');
