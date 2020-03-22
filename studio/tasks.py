740007a11c364d4f4d3b9128efb637452181eb9e
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-03-22 11:29:29 +0200

    Added live_journal_task
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- studio/tasks.py
+++ studio/tasks.py
@@ -1,9 +1,29 @@
 from Auto_posting_service.celery import app
+from .models import Post, SocialNetworkLiveJournal
+import xmlrpc.client as xmlrpclib
+import time
 
 @app.task
-def live_journal_task(a, b):
-    c = a + b
-    return c
+def live_journal_task(title, text, username, password):
+    ljsrv = xmlrpclib.ServerProxy(r"http://www.livejournal.com/interface/xmlrpc")
+    curtime = time.localtime()
+
+    data = {'ver':1,
+     'subject': title,
+     'year': curtime[0],
+     'mon': curtime[1],
+     'day': curtime[2],
+     'hour': curtime[3],
+     'min': curtime[4],
+     'security': 'private',
+     'event': text,
+     'username': username,
+     'password': password,
+     }
+
+    response = ljsrv.LJ.XMLRPC.postevent(data)
+    return response
+
 
 @app.task
 def telegram_task(a, b):
