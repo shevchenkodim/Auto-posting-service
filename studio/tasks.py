5b3baa2bca78e2bc5484843feb3854655556ea39
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-03-22 15:09:41 +0200

    Add try/except and retry to live_journal_task
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- studio/tasks.py
+++ studio/tasks.py
@@ -5,30 +5,32 @@ import time
 
 @app.task
 def live_journal_task(title, text, username, password, livejournal_id, post_id):
-    ljsrv = xmlrpclib.ServerProxy(r"http://www.livejournal.com/interface/xmlrpc")
-    curtime = time.localtime()
+    try:
+        ljsrv = xmlrpclib.ServerProxy(r"http://www.livejournal.com/interface/xmlrpc")
+        curtime = time.localtime()
 
-    data = {'ver':1,
-     'subject': title,
-     'year': curtime[0],
-     'mon': curtime[1],
-     'day': curtime[2],
-     'hour': curtime[3],
-     'min': curtime[4],
-     'security': 'private',
-     'event': text,
-     'username': username,
-     'password': password,
-     }
+        data = {'ver':1,
+         'subject': title,
+         'year': curtime[0],
+         'mon': curtime[1],
+         'day': curtime[2],
+         'hour': curtime[3],
+         'min': curtime[4],
+         'security': 'private',
+         'event': text,
+         'username': username,
+         'password': password,
+         }
+        response = ljsrv.LJ.XMLRPC.postevent(data)
 
-    response = ljsrv.LJ.XMLRPC.postevent(data)
-    post = Post.objects.get(id=post_id)
-    post.live_journal_result = True
-    post.save()
-    livejournal = SocialNetworkLiveJournal.objects.get(id=livejournal_id)
-    livejournal.connect_result = True
-    livejournal.save()
-
+        post = Post.objects.get(id=post_id)
+        post.live_journal_result = True
+        post.save()
+        livejournal = SocialNetworkLiveJournal.objects.get(id=livejournal_id)
+        livejournal.connect_result = True
+        livejournal.save()
+    except Exception as exc:
+        raise self.retry(exc=exc, countdown=60, max_retries=3)
     return response
 
 
